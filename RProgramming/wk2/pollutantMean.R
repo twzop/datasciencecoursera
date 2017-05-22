b470ab559d343e341b2d9241fe8a9aba66c67808
Leo McKenzie <solocomputerservices@yahoo.com>
2017-05-22 19:39:52 -0400

    Updated path for files.

---
+++ RProgramming/wk2/pollutantMean.R
@@ -0,0 +1,14 @@
+##  =============================
+##  Pollution Mean Function
+##  =============================
+  
+##
+##  Pollution Mean Function
+##  --------------------------
+##  This function calculates the mean of a pollutant (sulfate or nitrate)
+##  across a specified list of monitors.  It takes (3) arguments:
+##  'directory', 'pollutant', and 'id'.  Given a vector monitor ID
+##  number, pollutant mean of the pollutant across all of the monitors,
+##  ignoring any missing vales coded as NA.
+##
+
