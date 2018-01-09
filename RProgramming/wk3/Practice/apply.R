a166218168e492deb72fab272f8850d9512333e7
Leo McKenzie <solocomputerservices@yahoo.com>
2018-01-08 19:56:37 -0500

    * apply practice * built-in functions col-row-sum-means functions

--- RProgramming/wk3/Practice/apply.R
+++ RProgramming/wk3/Practice/apply.R
@@ -6,9 +6,24 @@
 # array
 # --------------------------------------------------------------
 # * Often used to apply a function to rows or cols of a matrix
-
+#
 # * Can be used with general arrays (taking AVG of matricies)
-
+#
 # * No faster than writing a loop, but works in one line of code
 # --------------------------------------------------------------
+
+# Show structure of apply() function
+str(apply)
+
+# Let's use apply on a matrix of 10 cols and 20 rows of 200 
+# random normal values
+x <- matrix(rnorm(200), 20, 10)
+print(x)
+# By specifying 2, you are selecting all cols in the matrix and 
+# omitting dimension 1 -- which are the rows.  We see means of 
+# all cols in the matrix with a vector as output
+apply(x, 2, mean)
+
+# Similarily, we can show sums of each row and not cols
+apply(x, 1, sum)
 
---
+++ RProgramming/wk3/Practice/col-row-sums-and-means.R
@@ -0,0 +1,17 @@
+# ============================================
+# Col/Row sums and means built-in functions
+# ============================================
+
+# In stead of using apply to calc the means and sums of a 
+# matrix, you can use the following built-in R functions
+# that are optimized to do this....
+#
+# ---------------------------------
+# *rowSums = apply(x, 1, sum)
+#
+# * rowMeans = apply(x, 1, mean)
+#
+# * colSums = apply(x, 2, sum)
+#
+# * colMeans = apply(x, 2, mean)
+# ---------------------------------
