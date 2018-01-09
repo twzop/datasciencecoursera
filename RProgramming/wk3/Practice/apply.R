2a288e27f5a1eb66c3562627c0a38896e0e21a1c
Leo McKenzie <solocomputerservices@yahoo.com>
2018-01-08 19:45:02 -0500

    * Finish sapply practice * Created apply practice

---
+++ RProgramming/wk3/Practice/apply.R
@@ -0,0 +1,14 @@
+# =============
+# Using apply
+# =============
+
+# apply is used to evaluate a function over the margins of an 
+# array
+# --------------------------------------------------------------
+# * Often used to apply a function to rows or cols of a matrix
+
+# * Can be used with general arrays (taking AVG of matricies)
+
+# * No faster than writing a loop, but works in one line of code
+# --------------------------------------------------------------
+
--- RProgramming/wk3/Practice/sapply.R
+++ RProgramming/wk3/Practice/sapply.R
@@ -5,3 +5,15 @@
 # sapply is a variant of lappy that simplifies the result of 
 # lapply if possible (lists to vectors as result type)
 
+# So, when we used the following with lapply to get a list back 
+# with all of the different subsets, sapply would have given us 
+# the same output, but in a single vector
+
+x <- list(a = 1:4, b = rnorm(10), c = rnorm(20, 1), d = rnorm(100, 5))
+lapply(x, mean)
+str(x) # x is a list
+
+# now apply sapply to this same list
+# x stays a list, but the output is a single vector
+sapply(x, mean)
+
